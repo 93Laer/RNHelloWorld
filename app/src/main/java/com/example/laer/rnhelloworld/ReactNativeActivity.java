c0e535db3c2a603f5142d6ada73f7eb0de2a9f6e
laitianbing <laitianbing@pand-auto.com>
2018-03-01 18:00:13 +0800

    提交项目

---
+++ app/src/main/java/com/example/laer/rnhelloworld/ReactNativeActivity.java
@@ -0,0 +1,12 @@
+package com.example.laer.rnhelloworld;
+
+import com.facebook.react.ReactActivity;
+
+public class ReactNativeActivity extends ReactActivity {
+
+    @Override
+    protected String getMainComponentName() {
+        return "RNHelloWorld";
+    }
+
+}
